863050b31a88a00b9dccedd74fff568baaed4803
QiuSYan <halo-402@outlook.com>
2025-05-31 09:37:15 +0800

    Update fonts.ts

--- app/fonts.ts
+++ app/fonts.ts
@@ -1,7 +1,7 @@
 import localFont from 'next/font/local'
 
 export const zpix = localFont({
-  src: '../public/fonts/zpix.ttf',
+  src: '../public/fonts/srweb.ttf',
   variable: '--font-zpix',
   display: 'swap',
 }) 
